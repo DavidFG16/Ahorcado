d1f66047eeeb30a55d45b9ee036e39412a62aee7
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-04-21 17:02:11 -0500

    feat: :sparkles: Creación de array de palabras como base de datos de las palabras del horcado

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-render(){
-    const buttonContainer = document.createElement('div')
-    buttonContainer.classList.add('button-container');   
-}
+const palabras = [
+    "javascript","ahorcado","computadora","teclado","raton","pantalla",
+    "internet","codigo","variable","funcion","programa","sintaxis",
+    "bucle","condicional","navegador","desarrollador","depurar","framework",
+    "backend","frontend","servidor","cliente","base","datos","objeto",
+    "clase","modulo","evento","asincrono","promesa","recurso","memoria",
+    "logica","algoritmo","estructura","vector", "matriz","array","string",
+    "booleano","numero","caracter","constante","lenguaje","ingeniero","software",
+    "hardware","bit","byte"
+  ];
+
+  
